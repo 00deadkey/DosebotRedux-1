a3376807449a683541c80a2f7f6fd70743a7608c
Felix <deadkey@kernelpanic.systems>
2021-10-08 21:11:04 -0400

    Replaced old youtube shuffler with temporary solution.

--- commands/randomtdc.js
+++ commands/randomtdc.js
@@ -2,7 +2,7 @@
 exports.run = (client, message, args) => {
   let response = `<@${
     message.author.id
-  }> - Enjoy a random episode of The Drug Classroom: <https://kek.gg/u/LP7h>`;
+  }> - Enjoy a random episode of The Drug Classroom: <https://youtube-playlist-randomizer.bitbucket.io/?pid=PLxIaqHsY6XLdSwXhdK44mOB0EEdklvbdj&autostart>`;
 
   message.channel.send(response).catch(console.error);
 };
